8c4eeef7e64cb62ca6f31fc39732cba2c5d2d85e
RibiZombie <151091236+RibiZombie@users.noreply.github.com>
2024-10-14 20:48:13 -0300

    Add files via upload

---
+++ Billetes2024/Billetes2024/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetes2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> billetes = new List<int>();
+            billetes.Add(100000);
+            billetes.Add(50000);
+            billetes.Add(20000);
+            billetes.Add(10000);
+            billetes.Add(5000);
+            billetes.Add(2000);
+            billetes.Add(1000);
+            billetes.Add(500);
+            billetes.Add(100);
+            billetes.Add(50);
+            billetes.Add(10);
+            billetes.Add(5);
+            billetes.Add(1);
+
+            int monto;
+            int cantidad;
+            Console.Write("Ingrese un monto: ");
+            monto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Para " + monto + "Gs. se nesecita: ");
+
+            foreach (int billete in billetes)
+            {
+                cantidad = monto / billete;
+                if (cantidad > 0)
+                {
+                    Console.WriteLine(cantidad + (billete < 2000 ? " moneda(s) " : " billete(s) de ") + billete + " Gs. ");
+                }
+                monto = monto - (billete * cantidad);
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ Puntajes/Puntajes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Puntajes
+{
+    class AlumnosPuntaje
+    {
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public int Puntaje { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<AlumnosPuntaje> alumnos = new List<AlumnosPuntaje>();
+
+            // Primer alumno
+            AlumnosPuntaje alumno1 = new AlumnosPuntaje();
+            alumno1.Apellido = "Ramirez";
+            alumno1.Nombre = "Elson";
+            alumno1.Puntaje = 90;
+            alumnos.Add(alumno1);
+
+            // Segundo alumno
+            AlumnosPuntaje alumno2 = new AlumnosPuntaje();
+            alumno2.Apellido = "Rios";
+            alumno2.Nombre = "Silvana";
+            alumno2.Puntaje = 95;
+            alumnos.Add(alumno2);
+
+            // Imprimir tabla de puntajes
+            Console.Clear();
+            Console.WriteLine("+-------------------------+-------------------------+--------+");
+            Console.WriteLine("| Apellido                | Nombre                  | Puntaje|");
+            Console.WriteLine("+-------------------------+-------------------------+--------+");
+
+            foreach (AlumnosPuntaje ap in alumnos)
+            {
+                Console.WriteLine($"| {ap.Apellido,-23} | {ap.Nombre,-23} | {ap.Puntaje,6} |");
+            }
+
+            Console.WriteLine("+-------------------------+-------------------------+--------+");
+
+            Console.ReadKey();
+        }
+    }
+}
